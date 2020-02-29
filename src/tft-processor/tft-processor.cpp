2a20d39593658f6d4384e31d4ba90b2ad8597372
CPowers <crp5488@rit.edu>
2020-02-29 14:20:35 -0500

    Should be fully working

--- src/tft-processor/tft-processor.cpp
+++ src/tft-processor/tft-processor.cpp
@@ -13,86 +13,78 @@ void TFT_PROCESSOR::updateScreen()
     myDisplay.updateScreen();
 }
 
-TFT_PROCESSOR::TFT_PROCESSOR(DASH_CONTROLLER_INTF *dashController) : accumText(0,0,0,RA8875_BLUE, RA8875_RED,"data = 000"),
-motorSpeed(0,90,0,RA8875_RED, RA8875_RED,"Motor Speed = 000"), busVoltage(0,300,0,RA8875_WHITE, RA8875_RED,"Bus Voltage = 000"),
-busVoltRect(300,0,200,20,RA8875_BLUE),
-outputVoltage(1,0,30,RA8875_WHITE, RA8875_RED,"Output Voltage = 000"), greenRect(0,0,85,20,RA8875_GREEN), whiteRect(90,0,150,20,RA8875_CYAN),
-myDashController(dashController), myDisplay(10,9){}
+TFT_PROCESSOR::TFT_PROCESSOR(DASH_CONTROLLER_INTF *dashController) : myDashController(dashController), 
+myDisplay(10,9),
+faultText(0,0,0,RA8875_WHITE, RA8875_RED,"No Faults"), faultTextRect(0,0,85,20,RA8875_GREEN),
+motorSpeed(0,90,0,RA8875_WHITE, RA8875_RED,"Motor Speed = 000"),  motorSpeedRect(87,0,150,20,RA8875_RED),
+busVoltage(0,300,0,RA8875_WHITE, RA8875_RED,"Bus Voltage = 000"),busVoltRect(295,0,150,20,RA8875_BLUE),
+outputVoltage(0,0,30,RA8875_WHITE, RA8875_RED,"Output Voltage = 000"), outputVoltageRect(0,28,140,20,RA8875_BLACK),
+maxTemp(1,205,225,RA8875_WHITE, RA8875_RED,"Tmax: 000"), maxTempRect(295,223,60,40,RA8875_BLACK),
+packVoltage(1,25,225,RA8875_WHITE, RA8875_RED,"Vtotal: 000"), packVoltageRect(168,223,60,40,RA8875_BLACK),
+batteryPercentage(1,0,60,RA8875_WHITE, RA8875_RED,"Battery% = 100"), batteryPercentageRect(175,58,50,40,RA8875_BLACK),
+lapNumber(1,250,60,RA8875_WHITE, RA8875_RED,"Lap: 0"), lapNumberRect(318,58,50,45,RA8875_BLACK),
+batteryPerLap(2,0,0,RA8875_WHITE, RA8875_RED,"Bat/Lap: 0"), batteryPerLapRect(200,0,125,50,RA8875_BLACK),
+waterTemp(0,150,100,RA8875_WHITE, RA8875_RED,"Twater: 0"), waterTempRect(148,98,175,20,RA8875_BLACK)
+{ 
+    this->lap = 0;
+    this->batteryBeforeLap = 100;
+    this->batteryPercent = 100;
+ }
 
 void TFT_PROCESSOR::initializeCallbacks()
 {
     //create callbacks and then register them
     //Create elements and send pointer to addElemnt to register it
 
-    Serial.printf("Original text address=%p, this=%p\n", &this->accumText, this);
-    this->myDisplay.addElement(&accumText);
-    this->myDisplay.addElement(&greenRect);
-    this->myDisplay.addElement(&motorSpeed);
-    this->myDisplay.addElement(&whiteRect);
-    this->myDisplay.addElement(&busVoltage);
-    this->myDisplay.addElement(&busVoltRect);
-    this->myDisplay.addElement(&outputVoltage);
+    // this->myDisplay.addElement(&faultText);
+    // this->myDisplay.addElement(&faultTextRect);
+    // this->myDisplay.addElement(&motorSpeed);
+    // this->myDisplay.addElement(&motorSpeedRect);
+    // this->myDisplay.addElement(&busVoltage);
+    // this->myDisplay.addElement(&busVoltRect);
+    // this->myDisplay.addElement(&outputVoltage);
+    // this->myDisplay.addElement(&outputVoltageRect);
     
+    this->myDisplay.addElement(&maxTemp);
+    this->myDisplay.addElement(&maxTempRect);
+    // this->myDisplay.addElement(&packVoltage);
+    // this->myDisplay.addElement(&packVoltageRect);
+    // this->myDisplay.addElement(&batteryPercentage);
+    // this->myDisplay.addElement(&batteryPercentageRect);
+    this->myDisplay.addElement(&lapNumber);
+    this->myDisplay.addElement(&lapNumberRect);
+    this->myDisplay.addElement(&batteryPerLap); //Instead of updating every cycle, try not adding to elements array but only update when get message?
+    this->myDisplay.addElement(&batteryPerLapRect);
+    this->myDisplay.addElement(&waterTemp);
+    this->myDisplay.addElement(&waterTempRect);
+
+    // batteryPerLapRect.updateElement(myDisplay.getDisplayDriver());
+    // batteryPerLap.updateElement(myDisplay.getDisplayDriver());
+    // lapNumberRect.updateElement(myDisplay.getDisplayDriver());
+    // lapNumber.updateElement(myDisplay.getDisplayDriver());
     
-    
-
-
-    Serial.printf("Address of accum text local=%p", &accumText);
-    // myDashController->registerCallback(0xAB, [&] (etl::array<uint8_t, 8> const &data) {
-    //     // char text[8];
-    //     // for(int i = 0; i <=7; i++){
-    //     //     text[i] = data[i];
-    //     //     text[i] = i;
-    //     // }
-
-    //     char textString[50] = "data = changed";
-
-    //     //sprintf(textString, "data = ", text);
-
-    //     //Serial.println("here123");
-    //     //If accumulator temp is 16 bit value, send 16 bits ntoh function to corrtect endianess
-    //     Serial.printf("Address of accum text we are calling=%p\n", &accumText);
-    //     accumText.updateText(textString);
-    //     });
-    
-    myDashController->registerCallback();
-    //NonMemberRegisterCallbacks(myDashController, this);
+    //Register callbacks. Callbacks must be registered in DASH_CONTROLLER::registerCallbacks
+    //for callbacks to be called
+    myDashController->registerCallback(); 
 
 }
 
-// void NonMemberRegisterCallbacks(DASH_CONTROLLER_INTF *dashController, TFT_PROCESSOR *processor){
-//     dashController->registerCallback(0xAB, etl::delegate<void(etl::array<uint8_t, 8> const &)>::create<TFT_PROCESSOR, &TFT_PROCESSOR::updateAcumText>(*processor));
-// }
-
-void TFT_PROCESSOR::updateAcumText(etl::array<uint8_t, 8> const &data) {
-        //char text[8];
-        // for(int i = 0; i <=7; i++){
-        //     text[i] = data[i];
-        //     text[i] = i;
-        // }
-
-        //char textString[50] = "data = changed";
-        char text[MAX_STRING_SIZE];
-        uint16_t number = data[0] | (data[1] << 8) | (data[2] << 16);
-        //int num = number;
-        Serial.print("number = ");
-        Serial.println(number);
-
-        sprintf(text, "data = %d", number);
-
-        //sprintf(textString, "data = ", text);
-
-        //Serial.println("here123");
-        //If accumulator temp is 16 bit value, send 16 bits ntoh function to corrtect endianess
-        Serial.printf("Address of accum text we are calling=%p\n", &accumText);
-        accumText.updateText(text);
-        }
+void TFT_PROCESSOR::updateFaultText(etl::array<uint8_t, 8> const &data) {
+     
+    char text[MAX_STRING_SIZE];
+    uint16_t number = data[0] | (data[1] << 8) | (data[2] << 16);
+    Serial.print("number = ");
+    Serial.println(number);
+    sprintf(text, "Fault = %d", number);
+    //If accumulator temp is 16 bit value, send 16 bits ntoh function to corrtect endianess
+    faultText.updateText(text); 
+}
 
 void TFT_PROCESSOR::MotorPositionInformation(etl::array<uint8_t, 8> const &data){
     char motorSpeedNum[MAX_STRING_SIZE];
     uint16_t number = data[2] | (data[3] << 8);
     if(data[0] == 0){
-        greenRect.updateColor(RA8875_RED);
+        motorSpeedRect.updateColor(RA8875_RED);
     }
     sprintf(motorSpeedNum, "Motor Speed = %d", number);
     motorSpeed.updateText(motorSpeedNum);
@@ -103,9 +95,62 @@ void TFT_PROCESSOR::VoltageInfo(etl::array<uint8_t, 8> const &data){
     char outputVoltageNum[MAX_STRING_SIZE];
     uint16_t number = data[2] | (data[3] << 8);
     sprintf(outputVoltageNum, "Output Voltage = %d", number);
-    outputVoltage.updateText(outputVoltageNum);
+    outputVoltage.updateText(outputVoltageNum); 
+
     char busVoltageNum[MAX_STRING_SIZE];
     number = data[0] | (data[1] << 8);
     sprintf(busVoltageNum, "Bus Voltage = %d", number);
     busVoltage.updateText(busVoltageNum);
+}
+
+void TFT_PROCESSOR::AccumTemp(etl::array<uint8_t, 8> const &data){
+    char maxTempNum[MAX_STRING_SIZE];
+    uint16_t number = data[4];
+    sprintf(maxTempNum, "Tmax: %d", number);
+    maxTemp.updateText(maxTempNum);
+}
+
+void TFT_PROCESSOR::AccumVoltage(etl::array<uint8_t, 8> const &data){
+    char packVoltageNum[MAX_STRING_SIZE];
+    uint16_t number = data[0] | (data[1] << 8);
+    sprintf(packVoltageNum, "Vtotal: %d", number);
+    packVoltage.updateText(packVoltageNum);
+}
+
+void TFT_PROCESSOR::AccumCharge(etl::array<uint8_t, 8> const &data){
+    char batteryPercentNum[MAX_STRING_SIZE];
+    uint16_t number = data[0];
+    batteryPercent = number; //Update current battery percentage for battery/lap
+    sprintf(batteryPercentNum, "Battery  = %d", number);
+    Serial.println(batteryPercentNum);
+    batteryPercentage.updateText(batteryPercentNum);
+}
+
+void TFT_PROCESSOR::IncrementLap(etl::array<uint8_t, 8> const &data){
+    lap += 1;
+    char lapNum[MAX_STRING_SIZE];
+    uint16_t number = lap;
+    sprintf(lapNum, "Lap: %d", number);
+    lapNumber.updateText(lapNum); 
+
+
+    //Determine battery used last lap
+    char batPerLapNum[MAX_STRING_SIZE];
+    int difference = batteryPercent - batteryBeforeLap; //Calulate battery used
+    Serial.print("Differnce");
+    Serial.println(number);
+    Serial.print("Percent");
+    Serial.println(batteryPercent);
+    Serial.print("old");
+    Serial.println(batteryBeforeLap);
+    sprintf(batPerLapNum, "Bat/Lap: %d", difference);
+    batteryPerLap.updateText(batPerLapNum);
+    batteryBeforeLap = batteryPercent; //Set new starting percentage
+}
+
+void TFT_PROCESSOR::waterTempInfo(etl::array<uint8_t, 8> const &data){
+    char waterTempNum[MAX_STRING_SIZE];
+    uint16_t number = data[0]; //Change
+    sprintf(waterTempNum, "Twater: %d", number);
+    waterTemp.updateText(waterTempNum);
 }
