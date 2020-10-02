62b04ef9ca5489c2875ca3af51071506f90ac50e
CRP5488 <crp5488@rit.edu>
2020-10-01 20:24:19 -0400

    Pulling branch and then merging to master

--- src/main.cpp
+++ src/main.cpp
@@ -14,31 +14,31 @@ void setup()
   Serial.print("startingg");
   pinMode(START_BUTTON_PIN, INPUT);
   pinMode(START_SOUND_SIGNAL, OUTPUT);
-  pinMode(19, OUTPUT);
-  pinMode(16, OUTPUT); //for testing
+  //inMode(19, OUTPUT);
+  //pinMode(16, OUTPUT); //for testing
   // put your setup code here, to run once:
   Serial.begin(9600);
   controller.initialize();
-  digitalWrite(19, HIGH);
-  digitalWrite(16, HIGH); //for testing RTDS
+  //digitalWrite(19, HIGH);
+  //digitalWrite(16, HIGH); //for testing RTDS
   digitalWrite(START_SOUND_SIGNAL, LOW);
 }
 
 void loop()
 {
-  Serial.print("message recieved = ");
-  Serial.println(controller.driveReady);
-  Serial.print("Button = ");
-  Serial.println(digitalRead(START_BUTTON_PIN));
+  // Serial.print("message recieved = ");
+  // Serial.println(controller.driveReady);
+  // Serial.print("Button = ");
+  // Serial.println(digitalRead(START_BUTTON_PIN));
   if (controller.driveReady && (digitalRead(START_BUTTON_PIN) == HIGH))
   {
     digitalWrite(START_SOUND_SIGNAL, HIGH);
   }
-  digitalWrite(19, HIGH);
+  //digitalWrite(19, HIGH);
   controller.updateModel();
   delay(DELAY);
   //Serial.println("loop");
-  digitalWrite(19, LOW);
+  //digitalWrite(19, LOW);
   controller.updateView();
   // Serial.println("6");
   delay(DELAY);
