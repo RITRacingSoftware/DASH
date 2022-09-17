ba5d12db7d98cf27c645b0c0a3bba0d0aed54cd4
Akari Neukirch <mnn1262@rit.edu>
2022-09-17 10:38:10 -0400

    Removed some seemingly unnecessary files, hasn't exploded yet

--- can-processor/can-processor.cpp
+++
@@ -1,6 +0,0 @@
-#include "can-processor.h"
-
-bool CAN_PROCESSOR::readCAN(uint16_t & id, std::array<uint8_t, 8>)
-{
-    return false;
-}
--- can-processor/can-processor.h
+++
@@ -1,9 +0,0 @@
-#include "can-processor-intf.h"
-
-class CAN_PROCESSOR : public CAN_PROCESSOR_INTF
-{
-    public:
-    CAN_PROCESSOR() {}
-
-    bool readCAN(uint16_t & id, std::array<uint8_t, 8>) override;
-};
