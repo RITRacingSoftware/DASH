c33104c0895fc9c0ca12724e0cf0f80e35a2c86e
Akari Neukirch <mnn1262@rit.edu>
2022-10-27 21:09:51 -0400

    Deleted unused files

--- include/controller-intf.h
+++
@@ -1,9 +0,0 @@
-class CONTROLLER_INTF
-{
-public:
-    virtual void initialize() = 0;
-    virtual void updateView() = 0;
-    virtual void updateModel() = 0;
-    virtual void registerCallback() = 0; //Might need to change parameters
-    virtual void readyToDrive() = 0;
-};
