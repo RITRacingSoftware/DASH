b30bd9f2383031645a131788087e2c927d29334e
Citrous06 <71228417+Citrous06@users.noreply.github.com>
2024-10-29 10:33:03 -0400

    update submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,18 @@
 #include <Arduino.h>
 
-
 #include "data-manager.h"
 #include "display-manager.h"
 
-
-
-#include "lvgl.h"
-
-
-
 void setup()
-{	
-
+{
 	Serial.begin(9600);
 	Serial.printf("Starting!\n");
 
 	DataManager::init();
-	lv_init();
-
- 
 }
 
 void loop()
 {
-
 	DataManager::update();
 
 	delay(100);
